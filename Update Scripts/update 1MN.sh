2d4d69b2289a33e07b46f773f156e153e57eb5e0
KingricharDV#0439 <northerncommunity1@gmail.com>
2021-07-18 11:36:55 -1000

    corrected dir

--- Update Scripts/update 1MN.sh
+++ Update Scripts/update 1MN.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 cd ~
-cd /root/
+cd /root/nestegg/./nestegg
 ./nestegg-cli stop
 rm -rf nesteggd nestegg-cli nestegg-tx
 wget https://github.com/SirElven8/NestEGG/releases/1.5.0.0/nestegglin1.5.0.0.tar.gz
--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8122a6df51d669d937c9a05883964e75f7510f896d686f72956180c6485e1f66
-size 1428
+oid sha256:448ba9dfbd4df9a5cc65a0b9d60fe4cdc42d50d9474b3d09158a6870e7a8edac
+size 1422
